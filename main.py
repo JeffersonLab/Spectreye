486c806e7d17925004bfd48bc275dd5ed661462d
Will Savage <wsavage6316@gmail.com>
2022-06-27 15:28:59 -0400

    isolate all vertical lines in image

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import numpy as np
 import cv2
+import math
 
 class SpectrometerAngleEstimator(object):
     def __init__(self, source):
-        self.frame = cv2.imread(source) #TODO video support
+        self.frame = cv2.imread(source, 0) #TODO video support
 
     def angle_from_frame(self):
-        while 1:
+        while True:
             img = self.frame
 
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            lsd = cv2.createLineSegmentDetector(0)
+            lines = lsd.detect(img)[0]
+
+            print(type(lines[0]))
+            segments = []
+            for i in range(0, len(lines)):
+                l = lines[i]
+                if abs(l[0][0] - l[0][2]) < abs(l[0][1] - l[0][3]):
+                    segments.append(lines[i])
+
+            img = lsd.drawSegments(img, np.asarray(segments))
 
             cv2.imshow("Detector", img)
-            wait = cv2.waitKey(0)
-            if wait & 0xFF == ord('q'):
+            key = cv2.waitKey(1)
+            if key == ord('q'):
                 break
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    model = SpectrometerAngleEstimator("test1.png")
+    model = SpectrometerAngleEstimator("test2.jpg")
     model.angle_from_frame()
